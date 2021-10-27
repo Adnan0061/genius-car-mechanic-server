740bd18273588577805bce10bba96ca8b2fe88e5
Adnan0061 <adnan.ahmed0061@gmail.com>
2021-10-28 00:54:28 +0600

    genius-car-mechanic-server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://adnan0061:a00ptwa00@cluster0.70s8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.70s8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
